bf40bb48fb7127a7f9d1a0b3672348485ee82cb9
Luqman Al-Hakim <137667685+mathakim29@users.noreply.github.com>
2025-06-24 13:50:44 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,170 @@
+let groupManager = new MoveableHelper.GroupManager([]);
+let targets = [];
+let moveableRef = null;
+let selectoRef = null;
+const cubes = [];
+
+for (let i = 0; i < 3; ++i) {
+  cubes.push(i);
+}
+const setSelectedTargets = (nextTargetes) => {
+  selectoRef.setSelectedTargets(nextTargetes.flat());
+
+  setTargets(nextTargetes);
+};
+
+const element$0 = document.querySelector(`[data-croffle-ref="element$0"]`);
+const element$1 = document.querySelector(`[data-croffle-ref="element$1"]`);
+const element$2 = document.querySelector(`[data-croffle-ref="element$2"]`);
+const element$3 = document.querySelector(`[data-croffle-ref="element$3"]`);
+const element$4 = document.querySelector(`[data-croffle-ref="element$4"]`);
+
+element$3.innerHTML = cubes
+  .map((i) => {
+    return `<div x=${i} class="cube" autocomplete="off" contenteditable="true">${i}</div>`;
+  })
+  .join("\n");
+
+const containerWidth = element$3.getBoundingClientRect().width;
+const containerheight = element$3.getBoundingClientRect().height;
+
+moveableRef = new Moveable(element$3, {
+  draggable: true,
+  scalable: true,
+  rotatable: true,
+  snappable: true,
+  snapCenter: true,
+  snapVertical: true,
+  snapHorizontal: true,
+  snapElement: true,
+  snapGap: true,
+  keepRatio: true,
+  verticalGuidelines: [
+    { pos: containerWidth / 2, className: "container-lines" },
+  ],
+  horizontalGuidelines: [
+    { pos: containerheight / 2, className: "container-lines" },
+  ],
+  elementGuidelines: [...document.querySelectorAll(".cube"), element$3],
+  bounds: element$3,
+});
+
+// locked objects test
+["dragStart", "scaleStart", "rotateStart"].forEach((eventName) => {
+  moveableRef.on(eventName, (e) => {
+    if (e.target.classList.contains("locked")) {
+      e.stop();
+    }
+  });
+});
+
+selectoRef = new Selecto({
+  container: document.querySelector(`[data-croffle-ref="selectoRef"]`),
+  dragContainer: window,
+  selectableTargets: [".selecto-area .cube"],
+  hitRate: 0,
+  selectByClick: true,
+  selectFromInside: false,
+  toggleContinueSelect: ["shift"],
+  ratio: 0,
+});
+function setTargets(nextTargets) {
+  targets = nextTargets;
+  moveableRef.target = targets;
+}
+element$1.addEventListener("click", () => {
+  const nextGroup = groupManager.group(targets, true);
+  if (nextGroup) {
+    setTargets(nextGroup);
+  }
+});
+element$2.addEventListener("click", () => {
+  const nextGroup = groupManager.ungroup(targets);
+  if (nextGroup) {
+    setTargets(nextGroup);
+  }
+});
+moveableRef.on("drag", (e) => {
+  e.target.style.transform = e.transform;
+});
+
+moveableRef.on("scale", (e) => {
+  e.target.style.transform = e.transform;
+});
+
+moveableRef.on("scaleGroup", (e) => {
+  e.target.style.transform = e.transform;
+});
+
+moveableRef.on("clickGroup", (e) => {
+  if (!e.moveableTarget) {
+    setSelectedTargets([]);
+    return;
+  }
+  if (e.isDouble) {
+    const childs = groupManager.selectSubChilds(targets, e.moveableTarget);
+    setSelectedTargets(childs.targets());
+    return;
+  }
+  if (e.isTrusted) {
+    selectoRef.clickTarget(e.inputEvent, e.moveableTarget);
+  }
+});
+
+selectoRef.on("dragStart", (e) => {
+  const moveable = moveableRef;
+  const target = e.inputEvent.target;
+  const flatted = targets.flat();
+
+  if (
+    target.tagName === "BUTTON" ||
+    moveable.isMoveableElement(target) ||
+    flatted.some((t) => t === target || t.contains(target))
+  ) {
+    e.stop();
+  }
+  e.data.startTargets = targets;
+});
+selectoRef.on("select", (e) => {
+  const { startAdded, startRemoved, isDragStartEnd } = e;
+
+  if (isDragStartEnd) {
+    return;
+  }
+  const nextChilds = groupManager.selectSameDepthChilds(
+    e.data.startTargets,
+    startAdded,
+    startRemoved,
+  );
+
+  setSelectedTargets(nextChilds.targets());
+});
+selectoRef.on("selectEnd", (e) => {
+  const { isDragStartEnd, added, removed, inputEvent, isClick } = e;
+  const moveable = moveableRef;
+  if (isDragStartEnd) {
+    inputEvent.preventDefault();
+    moveable.waitToChangeTarget().then(() => {
+      moveable.dragStart(inputEvent);
+    });
+  }
+  let nextChilds;
+
+  if (isDragStartEnd || isClick) {
+    nextChilds = groupManager.selectCompletedChilds(targets, added, removed);
+  } else {
+    nextChilds = groupManager.selectSameDepthChilds(targets, added, removed);
+  }
+  e.currentTarget.setSelectedTargets(nextChilds.flatten());
+  setSelectedTargets(nextChilds.targets());
+});
+
+const elements = selectoRef.getSelectableElements();
+
+const observer = new ResizeObserver(() => {
+  if (moveableRef) moveableRef.updateRect();
+});
+
+document.querySelectorAll(".cube").forEach((el) => observer.observe(el));
+
+groupManager.set([], elements);
---
+++ index.php
@@ -0,0 +1,41 @@
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
+
+  <button data-croffle-ref="element$1">Group</button>
+  &nbsp;
+  <button data-croffle-ref="element$2">Ungroup</button>
+
+
+<div class="root">
+    <div
+        class="container"
+        data-croffle-ref="element$0"
+    >
+        <script src="https://daybrush.com/moveable/helper/release/latest/dist/helper.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moveable/0.53.0/moveable.min.js" integrity="sha512-gFIuV9WCEJeWYkY1ZdJXugypot9ooEtwJf6U8In5JR6z5ZvV1xAvAQe9mQ7IYBXiF9ICXyiCeqgCJzqf64wh7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/selecto/1.26.3/selecto.min.js" integrity="sha512-LyYhmSJ2wilC9RjVXIC+Iq22nFVFTdTa5J/qGRJv42bmSEpHA0j1wQvdzu53MBG1Wuse7SP1BJp8lIzdBmsIDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link rel="stylesheet" href="index.css">
+        <script type="module" src="index.js"></script>
+
+
+
+
+            <div data-croffle-ref="selectoRef"></div>
+            <div
+                class="elements selecto-area"
+                data-croffle-ref="element$3"
+            >
+                <span
+            </div>
+            <div class="empty elements"></div>
+    </div>
+</div>
